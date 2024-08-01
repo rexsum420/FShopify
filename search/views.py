98b2e2c89c165cc17f160992f06ff21199affc1d
Rex Sum <rexsum420@gmail.com>
2024-08-01 11:45:20 -0500

    updated cart detail

--- search/views.py
+++ search/views.py
@@ -1,7 +1,18 @@
-from haystack.query import SearchQuerySet
 from django.shortcuts import render
+from products.models import Product
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 
 def search_view(request):
     query = request.GET.get('q', '')
-    search_results = SearchQuerySet().filter(content=query)
-    return render(request, 'search_results.html', {'products': search_results})
+    products = Product.objects.annotate(
+        relevance=Concat(
+            F('name'), 
+            Value(' '), 
+            F('description'), 
+            Value(' '), 
+            F('category__name')
+        )
+    ).filter(relevance__icontains=query).order_by('-relevance')
+
+    return render(request, 'search_results.html', {'products': products, 'authenticated': request.user.is_authenticated})
