f71bdb2d4323c847845dafa2bf437ddb07c95c46
Rex Sum <rexsum420@gmail.com>
2024-08-02 17:06:11 -0500

    removed double settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(7h+b9uv_x1b#pw+u_d2e*x+kt+@r8rv*yr#8rw244ta-uw27o'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
